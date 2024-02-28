327602d2d3ec5745b0c3fb8e158ce0ddd6ed2a9b
Hamdiata114 <diakitehamdiata@gmail.com>
2024-02-28 15:02:46 -0500

    Updated Main function to remove test code

--- main.py
+++ main.py
@@ -2,28 +2,7 @@ from Firebases import add_data_to_firestore,get_user_from_firestore,get_user_att
 from Notification import send_email
 from api import add_user,login
 def main():
-    # Adding Hamdiata Diak`ite as a user in the user collection.
-    username = 'hamdiatadiakite'
-    data_to_add = {
-        'name': 'Hamdiata Diakite',
-        'username': username,
-        'email': 'hamdiatadiakite@cmail.carleton.ca',
-        'phone number' : '819-213-6364',
-        'password' : 'SYSC3011'
-    }
-    
-    print(get_user_attribute(username,'email'))
-    print(login(username,'SYSC3011'))
-    
-    event_type = "door_unlocked"
-    timestamp = "2024-02-20T12:30:45Z"
-    details = {
-    "user_id": "user_id_123",
-    "door_id": "door_id_456"
-    }
-
-    add_event(event_type, timestamp, details)
-    
+   print("")
     
    
     
