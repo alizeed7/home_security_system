2bde7312d88f94979e0ee25b85fa34bee708d9fa
Yousef Hammad <yousef.h24@hotmail.com>
2024-04-01 00:55:15 -0400

    Update firebase.py

--- yousef test/firebase.py
+++ yousef test/firebase.py
@@ -1,5 +1,7 @@
 import pyrebase
 from datetime import datetime
+# import firebase_admin
+# from firebase_admin import credentials, storage
 
 
 config = { 
@@ -7,7 +9,10 @@ config = {
   "authDomain": "piguardian-bdb7e.firebaseapp.com", 
   "databaseURL": "https://piguardian-bdb7e-default-rtdb.firebaseio.com/", 
   "storageBucket": "piguardian-bdb7e.appspot.com" 
-} 
+}
+
+
+
 
 # Connect using your configuration 
 firebase = pyrebase.initialize_app(config) 
@@ -41,6 +46,34 @@ def add_event(event_type: str, details: dict):
    
   }
   new_event = db.child('Events').push(event_data)
-  
 
 
+
+def get_door_status():
+    '''
+    Function that retrieves the door status from the Firebase Realtime Database
+    
+    Returns:
+        doorStatus (bool): The current status of the door (True for open, False for closed)
+    '''
+    try:
+        doorStatus = db.child("doorStatus").get().val()
+        return doorStatus
+    except Exception as error:
+        print(f"Error retrieving door status: {error}")
+        return None
+    
+
+def set_register_face(username):
+    '''
+    Function to set the "registerFace" variable in Firebase Realtime Database to True.
+
+    Returns:
+        None
+    '''
+    try:
+        db.child("registerFace").set(True)
+        db.child("nameRequest").set(username)
+        print("Successfully set registerFace to True")
+    except Exception as error:
+        print(f"Error setting registerFace: {error}")
