76b1a081d2488baa32b16a2edd08f6f25a299982
Yousef Hammad <yousef.h24@hotmail.com>
2024-04-08 14:10:06 -0400

    removed unnessary folder

--- yousef-test/__init__.py
+++
@@ -1,3 +0,0 @@
-from .controller import add_user, login
-from .route import add_user_route
-
--- yousef-test/database/__init__.py
+++
@@ -1,2 +0,0 @@
-from .firestore_client import add_data_to_firestore, get_user_from_firestore,get_user_attribute
-from .firebase import add_event
